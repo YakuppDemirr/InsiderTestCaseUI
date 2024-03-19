184a8eeba71ad7266faaf285116c59142a4267ee
YakuppDemirr <yakuppdemirr01@gmail.com>
2024-03-19 19:35:36 +0300

    edit project

--- jenkins/script/jenkins.groovy
+++ jenkins/script/jenkins.groovy
@@ -1,8 +1,7 @@
 pipeline {
     agent any
     triggers {
-        github(gitHubPush())
-        github(gitHubPullRequest())
+        githubPush(branch: 'main')
     }
     stages {
         stage('Build') {
