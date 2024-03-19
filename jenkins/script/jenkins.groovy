ddfe7ef8b8a57fc82a852eff618f399c5d70a375
YakuppDemirr <yakuppdemirr01@gmail.com>
2024-03-19 20:18:42 +0300

    edit project

--- jenkins/script/jenkins.groovy
+++ jenkins/script/jenkins.groovy
@@ -1,25 +1,14 @@
 pipeline {
     agent any
-    triggers {
-        githubPush(branch: 'main')
-    }
     stages {
         stage('Build') {
             steps {
-                echo "Building the project..."
                 sh "mvn clean install"
             }
         }
         stage('Test') {
             steps {
-                echo "Running tests..."
                 sh "mvn test"
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo "Deploying the project..."
-                // Add deployment steps here
             }
         }
     }
