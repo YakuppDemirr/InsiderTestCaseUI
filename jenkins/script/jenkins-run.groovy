2ad2115fd6dd538c1c07eb163fde1bfaf2e10e88
YakuppDemirr <yakuppdemirr01@gmail.com>
2024-03-24 15:18:25 +0300

    edit project

--- jenkins/script/jenkins-run.groovy
+++ jenkins/script/jenkins-run.groovy
@@ -1,20 +1,21 @@
 pipeline {
     agent any
 
+    triggers {
+        scm '*/main'
+        pullRequest()
+    }
+
     stages {
         stage('Build') {
             steps {
-                // Maven'ı çalıştırmak için bat dosyasını çağır
-                echo 'Deneme - 1'
                 dir ('C:/Users/Yakup Demir/Desktop/InsiderTest') {
-                    echo 'Deneme - 2'
                     bat 'mvn clean install'
                 }
             }
         }
         stage('Test') {
             steps {
-                // Maven'ı testleri çalıştırmak için çağır
                 dir ('C:/Users/Yakup Demir/Desktop/InsiderTest') {
                     bat 'mvn test'
                 }
